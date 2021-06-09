4462a2789a5e5c785284c336c67207004f8daef7
based-hecks <85599807+based-hecks@users.noreply.github.com>
2021-06-09 02:01:09 -0500

    Update Modules.java

--- src/main/java/minegame159/meteorclient/systems/modules/Modules.java
+++ src/main/java/minegame159/meteorclient/systems/modules/Modules.java
@@ -531,7 +531,7 @@ public class Modules extends System<Modules> {
         add(new BetterChat());
         add(new BetterTab());
         add(new BookBot());
-        add(new DiscordPresence());
+
         add(new MessageAura());
         add(new MiddleClickFriend());
         add(new NameProtect());
